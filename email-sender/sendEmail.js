aa02e553a5a1143e47fc6e747f6b67a9be8a9bec
Prince0643 <tolentinochristian89@gmail.com>
2025-03-20 23:18:55 +0800

    System Online

--- email-sender/sendEmail.js
+++ email-sender/sendEmail.js
@@ -68,7 +68,6 @@ app.post('/send-email', (req, res) => {
     });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
